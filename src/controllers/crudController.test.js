68ae8a9b993b4b27b6dc456c7a9639bdc936a44d
Otávio Silva <otavio.ssilva22@gmail.com>
2022-03-10 16:22:57 -0300

    feat: jest tests and readme

--- src/controllers/crudController.test.js
+++ src/controllers/crudController.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../app.js');
+const { users } = require('../models/users');
 
 beforeAll(async () => {
     console.log('Iniciando TDD com jest!');
@@ -8,15 +9,35 @@ beforeAll(async () => {
 describe("Crud Controler", () =>{
 
     it("Deve retornar 400 se parâmetros inválidos", async()=>{
-        const response = await request(app).post('/create').send({});
+        let response = await request(app).post('/create').send({});
+        expect(response.statusCode).toBe(400);
+    
+        response = await request(app).post('/update').send({}); 
+        expect(response.statusCode).toBe(400);
 
-        const response_expected = {
-            error: 'Invalid params'
+        response = await request(app).post('/delete').send({}); 
+        expect(response.statusCode).toBe(400);
+
+        response = await request(app).get('/read'); 
+        expect(response.statusCode).toBe(400);
+    
+    });
+
+    it("Deve retornar 200 com os parâmetros válidos", async()=>{
+        jest.mock('../database/config.js');
+        jest.spyOn(users, 'create').mockImplementation(()=> new Promise((resolve) => resolve({
+            create: true
+        })))
+
+        let response_expected = {
+            success: true, 
+            statusCode: 200,
+            result: {
+                create: true
+            }
         }
 
-        expect(response_expected).toEqual({
-            error: 'Invalid params'
-        })
-
+        let response = await request(app).post('/create').send({"name": "teste", "email": "teste@mail.com"})
+        expect(response.body).toEqual(response_expected);
     })
 });
